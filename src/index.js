36d154a41a52d9c36179292a18707056b108b739
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-15 19:23:22 -0700

    still receiving error in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import serviceWorker from './serviceWorker';
+import  serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,3 +13,8 @@ ReactDOM.render(
   , document.getElementById('root'));
 
 serviceWorker.register();
+
+
+/*Failed to compile
+./src/index.js
+Attempted import error: './serviceWorker' does not contain a default export (imported as 'serviceWorker').*/
