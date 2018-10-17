455a27283585778319d850e26c9014d3e989f75f
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-16 19:18:26 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1> Bloc Jams</h1>
-        </header>
+         <header>
+           <nav>
+             <Link to='/'>Landing</Link>
+             <Link to='/library'>Library</Link>
+           </nav>
+           <h1>Bloc Jams</h1>
+         </header>
         <main>
-          Route exact path="/" component={Landing} />
-          Route path="/library" component={Library} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
         </main>
       </div>
     );
