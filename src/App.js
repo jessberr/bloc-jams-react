3532b5b111a919de191d15dbcc76af825d66adad
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-16 20:33:04 -0700

    with album

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
-
 class App extends Component {
   render() {
     return (
